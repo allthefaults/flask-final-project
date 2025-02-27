761f0e6c823752384953f2a975b7b8624ea44de4
Daniel <cdanisantos@gmail.com>
2025-02-27 10:43:56 -0500

    Unit testing

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -23,5 +23,20 @@ def emotion_detector(text_to_analyze):
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
+    # Return individual emotions
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        }
+    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+
     # Returning a dictionary containing sentiment analysis results
-    return formatted_response
+    return emotions
